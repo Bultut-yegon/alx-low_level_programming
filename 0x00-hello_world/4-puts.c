3c4143459b41116b6ae1e282a413efdf2b940b71
Bultut-yegon <bultut9785@gmail.com>
2023-03-16 04:13:10 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,5 +3,7 @@
  * main - Using function puts to print a certain program
  * Amazing
  */
-void main(){
-puts("Programming is like building a multilingual puzzle")};
+void main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+}
