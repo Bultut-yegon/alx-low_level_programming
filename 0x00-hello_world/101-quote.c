d8588764f6c0bb9d160da77f26e45fe873e49c28
Bultut-yegon <bultut9785@gmail.com>
2023-03-16 11:41:29 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Output a code without using reserved words,standard error
+ * Return: 1 (done)
+ */
+int main(void)
+{
+	fput("and that place of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
