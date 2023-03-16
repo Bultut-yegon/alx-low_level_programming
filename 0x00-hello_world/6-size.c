0ded58305a5ab791e1a137f4b262ad42dd4cea21
Bultut-yegon <bultut9785@gmail.com>
2023-03-16 05:32:55 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ char r;
 long int q;
 long long int t;
 float z;
-printf("The size of integer is: %lu.byte(s)\n", (unsigned int)sizeof(j));
-printf("The size of character is: %lu.byte(s)\n", (unsigned char)sizeof(r));
-printf("The size of long integer: %lu.byte(s)\n", (unsigned long)sizeof(q));
-printf("The size of L long integer: %lu.byte(s)\n", (unsigned long)sizeof(t));
-printf("The size of float is: %lu.byte(s)\n", (unsigned int)sizeof(z));
+printf("The size of integer is: %lu byte(s)\n", (unsigned int)sizeof(j));
+printf("The size of character is: %lu byte(s)\n", (unsigned char)sizeof(r));
+printf("The size of long integer: %lu byte(s)\n", (unsigned long)sizeof(q));
+printf("The size of L long integer: %lu byte(s)\n", (unsigned long)sizeof(t));
+printf("The size of float is: %lu byte(s)\n", (unsigned int)sizeof(z));
 return (0);
 }
